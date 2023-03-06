3f0292896573b2ac99180d5bf4c66f4cdde038f0
Alexis Pinzón <125093173+AlexisPinzoon@users.noreply.github.com>
2023-03-05 19:49:01 -0500

    Add files via upload
    Subir taller 1

---
+++ Form2.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Taller1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            cop = new TextBox();
+            button1 = new Button();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(253, 107);
+            label1.Name = "label1";
+            label1.Size = new Size(48, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Dolar";
+            // 
+            // cop
+            // 
+            cop.Location = new Point(353, 105);
+            cop.Name = "cop";
+            cop.Size = new Size(153, 23);
+            cop.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(294, 164);
+            button1.Name = "button1";
+            button1.Size = new Size(182, 47);
+            button1.TabIndex = 2;
+            button1.Text = "Convertir a pesos";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(204, 40);
+            label2.Name = "label2";
+            label2.Size = new Size(396, 30);
+            label2.TabIndex = 3;
+            label2.Text = "Convertir Dolares a Pesos Colombianos";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(cop);
+            Controls.Add(label1);
+            Name = "Form2";
+            Text = "Form2";
+            Load += Form2_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox cop;
+        private Button button1;
+        private Label label2;
+    }
+}
