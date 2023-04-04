7bc404d92fd83c09b2ce90d38f74f571e7a4c5bb
Alexis Pinzón <125093173+AlexisPinzoon@users.noreply.github.com>
2023-04-03 19:24:30 -0500

    Add files via upload

---
+++ Taller2/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesarrolloDeApps.Taller2
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private static readonly List<User> Users = new List<User>
+        {
+            new User { Nombre = "Juan Camilo", Id = "001-1234567-8", direccion = "Calle 123", celular = "555-1234", cumpleaños = new DateTime(1990, 01, 01) },
+            new User { Nombre = "Mariam Velez", Id = "002-1234567-9", direccion = "Calle 456", celular = "555-5678", cumpleaños = new DateTime(1985, 03, 15) },
+            new User { Nombre = "Luis Lunes", Id = "003-1234567-0", direccion = "Calle 789", celular = "555-9101", cumpleaños = new DateTime(1995, 05, 21) }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> Get()
+        {
+            return Ok(Users);
+        }
+    }
+
+    public class User
+    {
+        public string Nombre { get; set; }
+        public string Id { get; set; }
+        public string direccion { get; set; }
+        public string celular { get; set; }
+        public DateTime cumpleaños { get; set; }
+    }
+}
---
+++ Taller2/ValuesController1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesarrolloDeApps.Taller2
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private static readonly List<Product> Productos = new List<Product>
+        {
+            new Product { nombre = "Producto 1", Codigo = "001", Precio = 10.0, Estado = "Disponible" },
+            new Product { nombre = "Producto 2", Codigo = "002", Precio = 20.0, Estado = "Disponible" },
+            new Product { nombre = "Producto 3", Codigo = "003", Precio = 30.0, Estado = "No disponible" }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            return Ok(Productos);
+        }
+    }
+
+    public class Product
+    {
+        public string nombre { get; set; }
+        public string Codigo { get; set; }
+        public double Precio { get; set; }
+        public string Estado { get; set; }
+    }
+}
+
